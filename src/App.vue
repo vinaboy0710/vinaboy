75e7dbbf07ff231cf67353c3ea6d85819be77a47
vinaboy0710 <57781840+vinaboy0710@users.noreply.github.com>
2021-02-05 03:14:01 +0700

    Initialize patch-1

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+@import 'bulma';
+
+html, body {
+  background: transparent;
+  overflow: auto;
+}
+
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all .3s ease;
+}
+
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateY(10px);
+  opacity: 0;
+}
+</style>
